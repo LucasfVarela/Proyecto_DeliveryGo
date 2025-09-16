c4ba14bf1afbf50589c814575b4e820c2308fc8d
LucasfVarela <lucas.varela@hotmail.com.ar>
2025-09-16 20:25:55 -0300

    first

---
+++ Proyecto_DeliveryGo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proyecto_DeliveryGo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
