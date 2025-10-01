9836912c815ae798f8ea6852607d246fb744aa18
lautaronuccitelli <krebz234@gmail.com>
2025-10-01 04:03:28 -0300

    agrege checkout y solucione problema de archivos duplicados

--- Proyecto_DeliveryGo/Core/Facade/CheckoutFacade.cs
+++ Proyecto_DeliveryGo/Core/Facade/CheckoutFacade.cs
@@ -1,7 +1,22 @@
+using Proyecto_DeliveryGo.Core.Command;
+using Proyecto_DeliveryGo.Core.Strategy;
+using Proyecto_DeliveryGo.Core.Order;
+
 namespace Proyecto_DeliveryGo.Core.Facade
 {
     public class CheckoutFacade
     {
+        public CheckoutFacade(ICarritoPort carrito, IEnvioStrategy envioInicial, PedidoService pedidos)
+        {
 
+        }
+        private readonly ICarritoPort _carrito;
+        private readonly IEnvioStrategy _envioInicial;
+        private readonly PedidoService _pedidos;
+
+        public void AgregarItem(string sku, string nombre, decimal precio, int cantidad)
+        {
+            
+        }
     }
 }
---
+++ Proyecto_DeliveryGo/Core/Payment/IPagoBuilder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_DeliveryGo.Core.Payment_posible_eliminacion_
+{
+    class IPagoBuilder
+    {
+        
+    }
+}
